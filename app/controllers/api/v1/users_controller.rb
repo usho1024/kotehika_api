a47edf770877cc4f8ef6b12fda7571711fc0e4fc
usho1024 <usho1024@gmail.com>
2022-10-03 05:01:01 +0900

    Add authenticate service module

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,4 +1,6 @@
 class Api::V1::UsersController < ApplicationController
+  before_action :authenticate_active_user
+  
   def index
     users = User.all
     render json: users.as_json(only: [:id, :name, :email, :created_at])
