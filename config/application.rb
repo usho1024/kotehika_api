55b766a82dd866046a17a6fd7fb79c92396b2abc
usho1024 <usho1024@gmail.com>
2022-10-05 06:58:24 +0900

    Setup credentials for production

--- config/application.rb
+++ config/application.rb
@@ -37,6 +37,13 @@ module App
     # $LOAD_PATHにautoload pathを追加しない(Zeitwerk有効時false推奨)
     config.add_autoload_paths_to_load_path = false
 
+    # Cookieを処理するmeddlewareを追加
+    config.middleware.use ActionDispatch::Cookies
+
+    # Cookieのsamesite属性を変更する(Rails v6.1~, :strict, :lax, :none)
+    config.action_dispatch.cookies_same_site_protection =
+    ENV["COOKIES_SAME_SITE"].to_sym if Rails.env.production?
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
